5898fd42124092170e38ce7d26a39437ec495297
Kavisoorai33 <111513624+Kavisoorai33@users.noreply.github.com>
2024-08-17 11:44:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7256590511:AAEXChoAs7FMU7Zt2GqDgPACM72TDmvdYPg")
+API_ID = int(os.environ.get("API_ID", "28309170"))
+API_HASH = os.environ.get("API_HASH", "0f393a5ea55883459dfd93e52918cbe7")
+OWNER = int(os.environ.get("OWNER", "6026645957"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "filerenamer_speed_bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "funzone_official")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "2245092422"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://kavitech3333:Kavi@3333@telegrambot.7afkozc.mongodb.net/?retryWrites=true&w=majority&appName=TelegramBot")
+DB_NAME = os.environ.get("DB_NAME", "TelegramBot")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
